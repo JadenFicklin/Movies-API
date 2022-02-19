a837c5ab16cd3b4ea619ca8baf4e0adff7635142
JadenFicklin <jadendficklin@gmail.com>
2022-02-19 09:33:07 -0700

    created simple api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  //Variables | State
+  const [input, setInput] = useState("");
+  const [data, setData] = useState({ avatar_url: "" });
+  // https://api.github.com/users/
+
+  //Functions
+  const handleClick = () => {
+    axios.get(`https://api.github.com/users/${input}`).then((res) => {
+      setData(res.data);
+    });
+  };
+
+  //Return statement
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>App component</h2>
+      <input
+        placeholder="enter name here"
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <button onClick={handleClick}>Click Me </button>
+      <img src={data.avatar_url} />
     </div>
   );
 }
